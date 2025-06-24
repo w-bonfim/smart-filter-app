97cc39b1e8e73875e05924dc95e791435de9cead
Wellington <wellington.bonfim@hotmail.com>
2025-06-24 03:32:35 -0300

    style: adiciona layout base com TailwindCSS

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Filtro de Produtos</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    @livewireStyles
+</head>
+<body class="bg-gray-100 text-gray-800">
+
+    <div class="container mx-auto mt-10 p-4 bg-white rounded shadow">
+        @yield('content')
+    </div>
+
+    @livewireScripts
+</body>
+</html>
